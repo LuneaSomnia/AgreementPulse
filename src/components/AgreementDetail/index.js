d9947ef4bb04ceb3887ea4b6ed6372c5f9dc6f0f
LuneaSomnia <raymondtexas909@gmail.com>
2025-01-28 12:57:19 +0300

    Create index.js

---
+++ src/components/AgreementDetail/index.js
@@ -0,0 +1,4 @@
+export { default as PartiesView } from './PartiesView';
+export { default as TermsView } from './TermsView';
+export { default as SignaturesView } from './SignaturesView';
+export { default as ActionsView } from './ActionsView';
